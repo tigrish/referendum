9c21fc717b4c5c96859ab0edbe4e1a8efd141c8c
Christopher Dell <chris@tigrish.com>
2010-12-14 22:48:26 +0100

    Create proposals for the current_user

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,3 +1,9 @@
 class ProposalsController < InheritedResources::Base
   actions :all, :except => [:edit, :update, :destroy]
+  
+  def create
+    @proposal = Proposal.new(params[:proposal])
+    @proposal.user = current_user
+    create!
+  end
 end
--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -8,5 +8,25 @@ describe ProposalsController do
       response.should be_success
     end
   end
+  
+  describe "POST 'create'" do
+    before(:each) do
+      @user = User.generate!
+      controller.send(:current_uesr=, @user)
+    end
+    
+    def do_action
+      post :create, :proposal => {:title => '[title]', :description => '[description]'}
+    end
+    
+    it "should create a proposal" do
+      proc { do_action }.should change(Proposal, :count).by(1)
+    end
+    
+    it "should assign the current_user to the created proposal" do
+      do_action
+      assigns(:proposal).should == @user
+    end
+  end
 
 end

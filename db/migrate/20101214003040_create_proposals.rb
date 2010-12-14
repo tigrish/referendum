cf31ed79c6919707b199de0ee9c797541a04a7f9
Christopher Dell <chris@tigrish.com>
2010-12-14 01:37:36 +0100

    Created proposal model

---
+++ db/migrate/20101214003040_create_proposals.rb
@@ -0,0 +1,14 @@
+class CreateProposals < ActiveRecord::Migration
+  def self.up
+    create_table :proposals do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :proposals
+  end
+end
---
+++ spec/models/proposal_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Proposal do
+  should_belong_to(:user)
+
+  should_validate_presence_of(:title)
+  should_validate_presence_of(:description)
+  should_validate_presence_of(:user)
+end

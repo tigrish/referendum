21d2a71bbcd870ea053d579988f6acfbb63456d1
Christopher Dell <chris@tigrish.com>
2010-12-15 03:11:09 +0100

    Comment validates presence of body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :proposal
   belongs_to :user
-  
+
+  validates_presence_of :body
   validates_presence_of :proposal
   validates_presence_of :user
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Comment do
   should_belong_to :proposal
   should_belong_to :user
-  
+
+  should_validate_presence_of :body
   should_validate_presence_of :proposal
   should_validate_presence_of :user
 end

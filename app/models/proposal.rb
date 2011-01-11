e5fd3cbb0ed5df5ddffc0a96404ff22d2837871e
Christopher Dell <chris@tigrish.com>
2011-01-11 22:46:03 +0100

    Added a state field to proposals, with a default of "open"

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,7 +1,13 @@
 class Proposal < ActiveRecord::Base
+  include ActiveRecord::Transitions
+  
   belongs_to :user
   has_many   :comments
   has_many   :votes
   
   validates_presence_of :title, :description, :user
+  
+  state_machine do
+    state :open
+  end
 end

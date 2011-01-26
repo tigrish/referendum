a28e47ada87c07ae7582b18519dfc0d146f38db4
Christopher Dell <chris@tigrish.com>
2011-01-26 03:00:23 +0100

    Configure the production environment to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ Democracy::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_HOST'] }
 
   # Enable threaded mode
   # config.threadsafe!
